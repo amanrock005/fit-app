ee5fcacd74c90c15e56a6f60d9fa659e1aff3194
amanpreet singh <amanpreetsingh2524@gmail.com>
2022-07-01 08:30:18 +0530

    add files

---
+++ frontend/src/pages/About/About.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import './About.css'
+
+export default function About() {
+    return (
+        <div className="about-container">
+            <h1 className="about-heading">About</h1>
+            <div className="about-main">
+                <p className="about-content">
+                    This is an realtime AI based Yoga Trainer which detects your pose how well you are doing.
+                    In the 21st century, artificial intelligence (AI) and machine learning are becoming commonplace. 
+                    Both are becoming more important components of current workout routines. 
+                    AI has become intrinsically connected to health and fitness. 
+                    AI, according to technologists, improves everything. 
+                    When it comes to fitness, it has the potential to empower the app by dramatically increasing engagement, which may lead to long-term income. 
+                    The AI-enabled apps can give users a variety of advantages. It may save money for a fitness enthusiast, for example, because an artificial intelligence fitness trainer is less expensive than a human trainer, but joining a gym might be expensive or impossible to integrate into our busy life. 
+                    In addition, AI-powered fitness software may make a workout more interesting and enjoyable. 
+                    Here, we will mention top fitness apps which work on AI and machine learning models.
+                    
+
+                    We have trained the model in python because of tensorflowJS we can leverage the support of browser so I converted 
+                    the keras/tensorflow model to tensorflowJS.
+                </p>
+            </div>
+        </div>
+    )
+}
